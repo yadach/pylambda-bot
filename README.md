e909b7c54c09e3193dc2d0c8bcd7fdc2356cdd65
yadach <47210083+yadach@users.noreply.github.com>
2023-05-12 17:51:29 +0900

    fix build script

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ AWSのLambdaを用いたbot関数です。
 # 環境
 - Python3.9
 
+# 開発環境構築
+
+```bash
+$ poetry install
+```
+
 # パッケージのビルド方法
 
 ```bash
--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 set -u
 
 pkg_name="pylambda_bot"
-PYTHON_VERSION=3.7.16
 
-
-pkg_dir="${pkg_name}_zip2aws"
+pkg_dir="awspkg_${pkg_name}"
 pkg_zip="${pkg_dir}.zip"
 
 # make package directory
 rm -rf "${pkg_dir}"
-python3.7 -m pip install . -t "${pkg_dir}"
+source .venv/bin/activate
+python -m pip install . -t "${pkg_dir}"
 cp lambda_function.py "${pkg_dir}"
 cp config.yaml "${pkg_dir}"
 
